4cefa124d6cc62f60043ab93a35ebada00d8dcbc
Ryan Angilly <ryan@angilly.com>
2010-10-02 20:14:15 -0600

    some style, user-aware nav basics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def page_heading
+    @page_heading || '&nbsp'.html_safe
+  end
+
+  def aside_heading
+    @aside_heading || '&nbsp'.html_safe
+  end
+
+  def navigation_content
+    @navigation_content || render(:partial => 'shared/navigation')
+  end
+
 end
