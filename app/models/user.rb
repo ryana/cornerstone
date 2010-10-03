0c2ce186772350f78ceb408f985ba744109dcb32
Ryan Angilly <ryan@angilly.com>
2010-10-02 18:44:34 -0600

    INITIAL COMMIT

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# Based on
+# http://blog.bitzesty.com/mongodb-with-mongomapper-and-ruby-on-rails
+
+class User
+  include MongoMapper::Document
+  devise :authenticatable, :recoverable, :rememberable
+
+  key :email,  String
+  key :username,  String
+  key :comment_count, Integer
+  key :encrypted_password, String
+  key :password_salt, String
+  key :reset_password_token, String
+  key :remember_token, String
+  key :remember_created_at, Time
+  key :sign_in_count, Integer
+  key :current_sign_in_at, Time
+  key :current_sign_in_ip, String  
+  timestamps!
+  
+  RegEmailName   = '[\w\.%\+\-]+'
+  RegDomainHead  = '(?:[A-Z0-9\-]+\.)+'
+  RegDomainTLD   = '(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
+  RegEmailOk     = /\A#{RegEmailName}@#{RegDomainHead}#{RegDomainTLD}\z/i
+  
+  validates_length_of :email, :within => 6..100, :allow_blank => true
+  validates_format_of :email, :with => RegEmailOk, :allow_blank => true
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cornerstone::Application.config.secret_token = 'b99d514e9a94c3cdcc4158fc942511681ab896ba0435d681138b0153474f1e8c949276efcd0f9b350f4926be2b01efb4a4f280fcc06f291f58437486a49725c0'
